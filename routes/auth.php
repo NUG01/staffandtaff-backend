c6cbc146fd394082396603cc7442a7a4b35c74d9
N S <nskhiereli@gmail.com>
2023-03-16 10:44:04 +0400

    fix: routes

--- routes/auth.php
+++ routes/auth.php
@@ -10,13 +10,13 @@ use App\Http\Controllers\Auth\{
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest')->name('register');
-    Route::post('/login', [AuthenticatedSessionController::class, 'store'])->middleware('guest')->name('login');
-    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->middleware('guest')->name('password.email');
-    Route::post('/reset-password', [NewPasswordController::class, 'store'])->middleware('guest')->name('password.store');
-    Route::post('/verify-email', VerifyEmailController::class)->middleware(['auth', 'signed', 'throttle:6,1'])->name('verification.verify');
+    Route::post('/register', [RegisteredUserController::class, 'store'])->name('user.register');
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('user.login');
+    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
+    Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('password.store');
 });
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth')->name('logout');
+Route::post('/verify-email', VerifyEmailController::class)->middleware(['auth', 'signed', 'throttle:6,1'])->name('verification.verify');
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth')->name('user.logout');
 
 // Route::post('/forgot-password', fn () => response()->noContent());
 // Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->middleware('guest')->name('password.odd');
