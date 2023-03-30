9c6f9a1d60a66fe37f1a3839a944c75ff7f98de8
luka mkalavishvili <mkalavishvililuka00@gmail.com>
2023-03-30 14:43:10 +0400

    added name on user data

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -25,6 +25,7 @@ class UserResource extends JsonResource
 
         return [
             'id' => $this->id,
+            'name' => $this->name,
             'email' => $this->email,
             'verified' => boolval($this->email_verified_at),
             'type' => $query,
