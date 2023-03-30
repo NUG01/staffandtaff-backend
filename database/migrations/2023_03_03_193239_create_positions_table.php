e40bcec9c32292126d06037195e929aa73a2efdc
N S <nskhiereli@gmail.com>
2023-03-30 11:03:24 +0400

    position db fix

--- database/migrations/2023_03_03_193239_create_positions_table.php
+++ database/migrations/2023_03_03_193239_create_positions_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug');
+            $table->integer('hotel')->nullable();
+            $table->integer('restaurant')->nullable();
             $table->timestamps();
         });
     }
