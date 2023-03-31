826ea3b995af323fdfe9aa1a9b8099ff1c07a516
anri2407 <anrivarshanidze2407@gmail.com>
2023-03-31 15:29:49 +0400

    about us

--- app/Http/Controllers/Auth/AboutController.php
+++ app/Http/Controllers/Auth/AboutController.php
@@ -5,15 +5,22 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Auth\MailController;
+use Illuminate\Support\Facades\Auth;
 
 
 class AboutController extends Controller
 {
     public function store(Request $request)
     {
-        $userName = $request->name;
-        $userMail = $request->email;
-        $userSms = $request->sms;
+        if (Auth::user()){
+            $userName = Auth::user()->name;
+            $userMail = Auth::user()->email;
+            $userSms = $request->sms;
+        }else{
+            $userName = $request->name;
+            $userMail = $request->email;
+            $userSms = $request->sms;
+        }
 
 
         if($userName != null && $userMail != null && $userSms != null){
